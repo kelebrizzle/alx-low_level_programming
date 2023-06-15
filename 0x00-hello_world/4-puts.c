4cb49d6f7c679ca8b427c462857614cae3275eca
Kelechukwu <kelebrizzle@gmail.com>
2023-06-15 16:33:37 -0700

    fixed script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,6 +8,7 @@
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }
