a4946c6e0e3f21a9c19fc14605eaf99f1f527104
Kelechukwu <kelebrizzle@gmail.com>
2023-06-16 14:45:47 -0700

    add script

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+	if (c != 'e')
+	{
+		if (c != 'q')
+		{
+			putchar(c);
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
