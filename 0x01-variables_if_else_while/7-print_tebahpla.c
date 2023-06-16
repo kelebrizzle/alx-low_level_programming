2710dcb28bc085c6d48e31e2a13c4b7cfdafd2ee
Kelechukwu <kelebrizzle@gmail.com>
2023-06-16 14:55:54 -0700

    add script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+{
+	putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
