863e0b67a7a290d5f90f1017e74e96587585589c
Kelechukwu <kelebrizzle@gmail.com>
2023-06-28 10:54:39 -0700

    add script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet - Prints alphabets
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n');
+}
