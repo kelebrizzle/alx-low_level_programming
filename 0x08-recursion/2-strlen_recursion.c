8e88c893caf5ce82211f5e67809e60a20f00441c
Kelechukwu <kelebrizzle@gmail.com>
2023-07-05 12:31:41 -0700

    add script

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: string to be checked
+ * Return: integer
+*/
+void _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + (_strlen_recursion(s + 1)))
+	}
+	else
+	{
+		return (0);
+	}
+}
