f6c0273d8209713cc2969e1b1ceb4ddf6b2042d4
Kelechukwu <kelebrizzle@gmail.com>
2023-07-05 12:41:45 -0700

    fix script

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,8 +4,9 @@
  * @s: string to be checked
  * Return: integer
 */
-void _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
+
 	if (*s != '\0')
 	{
 		return (1 + (_strlen_recursion(s + 1)));
