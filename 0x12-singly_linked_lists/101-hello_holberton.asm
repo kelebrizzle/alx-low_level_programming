950999d2b921f6e0cf617ffce7766523b5a240d3
Kelechukwu <kelebrizzle@gmail.com>
2023-07-27 17:42:03 -0700

    fixed script

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,49 +3,25 @@
 ; Desc: 64-bit assembly program that prints
 ;       Hello, Holberton followed by a new line.
 
-extern printf
-
-section .text
-   global main
-
-main:
-   push rbp
-
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
-
-section .data
-   msg: db "Hello, Holberton", 0
-   fmt: db "%s", 10, 0; File: 101-hello_holberton.asm
-; Auth: Gedeon Obae Gekonge
-; Desc: 64-bit assembly program that prints
-;       Hello, Holberton followed by a new line.
-
-extern printf
-
-section .text
-   global main
-
-main:
-   push rbp
-
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
+extern printf, exit
 
 section .data
    msg: db "Hello, Holberton", 0
    fmt: db "%s", 10, 0
+
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi, fmt
+   mov rsi, msg
+   xor rax, rax ; Clear RAX (no vector registers used, equivalent to mov rax, 0)
+   call printf
+
+   pop rbp
+
+   xor rax, rax ; Return value 0 (success)
+   ret
+
